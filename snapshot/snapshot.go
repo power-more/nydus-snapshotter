7e0a285d9ad630b631f24bc51aacc36c5c8f9061
zhaoshang <zhaoshangsjtu@linux.alibaba.com>
2022-08-01 15:54:42 +0800

    Fix BlobMgr Remove
    Previously in the localfs backend mode, when blobMgr delete the blob
    corresponding to the snapshot, it mistakenly used the key (chainID)
    issued by containerd as the blob digest, resulting in only the first
    layer of blob being deleted.
    
    Now
    1. For the scenario of converting OCI to Nydus image, get the blob
    digest, in fact the snapshot key by label NydusDataLayer, the kv pair
    (NydusDataLayer --> snapshot key) has been set after the convert.
    2. For the scenario of full download of Nydus image, get the correct
    blob digest by label CRILayerDigest.
    
    Signed-off-by: zhaoshang <zhaoshangsjtu@linux.alibaba.com>

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -501,9 +501,10 @@ func (o *snapshotter) Remove(ctx context.Context, key string) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to remove")
 	}
-	var cleanupBlob bool
-	if _, ok := snap.Labels[label.NydusDataLayer]; ok {
-		cleanupBlob = true
+	var blobDigest string
+	_, cleanupBlob := snap.Labels[label.NydusDataLayer]
+	if cleanupBlob {
+		blobDigest = snap.Labels[label.CRILayerDigest]
 	}
 
 	if o.syncRemove {
@@ -524,7 +525,7 @@ func (o *snapshotter) Remove(ctx context.Context, key string) error {
 					}
 				}
 				if cleanupBlob {
-					if err := o.fs.CleanupBlobLayer(ctx, key, false); err != nil {
+					if err := o.fs.CleanupBlobLayer(ctx, blobDigest, false); err != nil {
 						log.G(ctx).WithError(err).WithField("id", key).Warn("failed to remove blob")
 					}
 				}
@@ -533,7 +534,7 @@ func (o *snapshotter) Remove(ctx context.Context, key string) error {
 	} else {
 		defer func() {
 			if cleanupBlob {
-				if err := o.fs.CleanupBlobLayer(ctx, key, true); err != nil {
+				if err := o.fs.CleanupBlobLayer(ctx, blobDigest, true); err != nil {
 					log.G(ctx).WithError(err).WithField("id", key).Warn("failed to remove blob")
 				}
 			}
